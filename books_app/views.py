45c9c28d1fef20e2c0a84eb25975d3de54807232
Kamronbek Atajanov <atajanovkamronbek2003@gmail.com>
2024-02-21 18:03:05 +0500

    third commit

--- books_app/views.py
+++ books_app/views.py
@@ -26,4 +26,6 @@ class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView): #
     
     
     
+
+
     
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,33 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library project Api",
+        default_version="v1",
+        description="library demo project",
+        terms_of_service="demo.com",
+        contact=openapi.Contact(email="atajanovkamronbek2003@gmail.com"),
+        license=openapi.License(name="demo license")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts_app.urls')),
     path('api/v1/', include('books_app.urls')),
+    
+    # Swagger & Redoc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
 
 if settings.DEBUG:
